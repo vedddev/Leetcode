aca983c1ff2a4b65b15886859887bb423d06dc42
Vedant Shelake <vedantshelake28@gmail.com>
2026-09-17 08:05:02 +0530

    Time: 3 ms (39.78%), Space: 19.6 MB (21.21%) - LeetHub

--- 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.py
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.py
@@ -3,10 +3,16 @@ class Solution:
         nums=sorted(nums)
         l=0
         r=len(nums)-1
-        min_value=float('inf')
+        avg=0
+        result=[]
         while l<r:
-            avg=(nums[l]+nums[r])/2
-            min_value=min(min_value,avg)
+            avg=nums[l]+nums[r]
+            avg/=2
+            result.append(avg)
             l+=1
             r-=1
+        min_value=result[0]
+        for i in range(1,len(result)):
+            if min_value>result[i]:
+                min_value=result[i]
         return min_value
